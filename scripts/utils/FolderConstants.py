1370af8bba05dd6609fe3a756784033f93aae39a
Mihkel <miku_149@hotmail.com>
2017-08-01 03:43:27 +0300

    CACHE_PATH parameeter lisatud FolderConstants klassi

--- scripts/utils/FolderConstants.py
+++ scripts/utils/FolderConstants.py
@@ -9,3 +9,5 @@ class FolderConstants:
     SAVE_PATH = os.path.abspath(
         os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'tests', "resources",
                      SAVE_FOLDER))
+
+    CACHE_PATH = os.path.join(SAVE_PATH, "tmp")
