19f219738b7b391a41e09ebc469c1832f45768c3
Mihkel <miku_149@hotmail.com>
2017-11-27 00:20:53 +0200

    MetaSubProcess'i save_results ja load_results

--- scripts/MetaSubProcess.py
+++ scripts/MetaSubProcess.py
@@ -4,3 +4,9 @@ from abc import ABCMeta, abstractmethod
 class MetaSubProcess(metaclass=ABCMeta):
     @abstractmethod
     def start_process(self): pass
+
+    @abstractmethod
+    def save_results(self): pass
+
+    @abstractmethod
+    def load_results(self): pass
